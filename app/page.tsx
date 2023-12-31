3b1528edb2d62a8d33af56e18ddbd6086bd49f57
ArifSanaullah <ak7896023@gmail.com>
2023-12-31 10:50:21 +0500

    search functionality

--- app/page.tsx
+++ app/page.tsx
@@ -7,23 +7,62 @@ import Image from "next/image";
 import { labelOrDefault } from "@/lib/labelOrDefault";
 import { useState } from "react";
 import classNames from "classnames";
-import { ArrowLeftIcon, ShoppingBagIcon } from "@heroicons/react/24/outline";
+import {
+  ArrowLeftIcon,
+  MagnifyingGlassIcon,
+  ShoppingBagIcon,
+} from "@heroicons/react/24/outline";
 import { Drawer } from "vaul";
 import { Bucket } from "@/components/bucket";
+import emptyCart from "@/assets/images/emptyCart.jpg";
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
   const [activeTab, setActiveTab] = useState("all");
 
+  const [query, setQuery] = useState("");
+
+  const filteredWithCategories =
+    activeTab === "all"
+      ? items
+      : items.filter((i) => i.categories.some((cat) => cat.id === activeTab));
+
+  const filteredItems = query.trim().length
+    ? filteredWithCategories.filter((i) =>
+        labelOrDefault(i.name).toLowerCase().includes(query.toLowerCase())
+      )
+    : filteredWithCategories;
+
   return (
     <div className="py-4 h-full px-4 sm:px-6 lg:px-8 bg-gray-40 flex-1">
       <div className="flex flex-col mx-auto max-w-7xl">
-        <div className="pb-4">
-          <div className="flex items-center gap-2 sm:gap-4">
-            <h1 className="text-xl font-bold">Hey, Danyal</h1>
-            <Image src={hand} alt="Waving emoji" width={32} height={32} />
+        <div className="flex items-center gap-4 sm:justify-between flex-col sm:flex-row pb-4">
+          <div className="flex-1 self-start">
+            <div className="flex items-center gap-2 sm:gap-4">
+              <h1 className="text-xl font-bold">Hey, Danyal</h1>
+              <Image src={hand} alt="Waving emoji" width={32} height={32} />
+            </div>
+            <p className="text-gray-500 text-sm">It&lsquo;s dinner time</p>
           </div>
-          <p className="text-gray-500 text-sm">It&lsquo;s dinner time</p>
+          <div className="flex-1 flex w-full">
+            <div className="relative flex flex-grow items-stretch focus-within:z-10">
+              <input
+                type="search"
+                name="query"
+                id="query"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                placeholder="Type item name to search..."
+                className="placeholder:text-xs block w-full rounded-none rounded-l-md border-0 py-1.5 pl-4 focus-visible:outline-primary text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
+              />
+            </div>
+            <button type="button" className="border rounded-r-md px-4">
+              <MagnifyingGlassIcon
+                className="h-5 w-5 text-gray-500"
+                aria-hidden="true"
+              />
+            </button>
+          </div>
         </div>
         <div className="mb-4 sm:mb-6 flex flex-nowrap overflow-x-auto gap-4">
           <div
@@ -66,11 +105,26 @@ export default function Home() {
             </div>
           ))}
         </div>
-        <div className="grid grid-cols-6 gap-4 sm:gap-6">
-          {items.map((item) => (
-            <Item key={item.id} item={item as MenuItem} />
-          ))}
-        </div>
+        {filteredItems.length > 0 ? (
+          <div className="grid grid-cols-6 gap-4 sm:gap-6">
+            {filteredItems.map((item) => (
+              <Item key={item.id} item={item as MenuItem} />
+            ))}
+          </div>
+        ) : (
+          <div className="flex items-center flex-1 justify-center">
+            <div className="flex flex-col items-center justify-center gap-4">
+              <Image
+                src={emptyCart}
+                className="rounded-full"
+                alt="Empty Cart"
+                width={0}
+                height={0}
+              />
+              <span>No Items found with the entered term</span>
+            </div>
+          </div>
+        )}
       </div>
       <Drawer.Root snapPoints={[1]} open={isOpen} onOpenChange={setIsOpen}>
         <Drawer.Trigger onClick={() => setIsOpen(true)}>
